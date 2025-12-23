a49a61d37e8886fb5a8f3a2a4aa3e1c58364df1e
WijekoonJanithBCS <wijekoonjanith72@gmail.com>
2025-12-23 13:41:06 +0530

    created & completed #2

--- src/main/java/ik/ikman/oop/students.java
+++ src/main/java/ik/ikman/oop/students.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 
 public class students {
     public static void main(String[] args) {
-        System.out.print("hello janith saranga wijekoon.....");
+        System.out.print("hello janith saranga wijekoon kandy.....");
     }
 
 
